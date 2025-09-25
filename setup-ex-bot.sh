c673db37ab088799a09a9c3561da99ed4883db2d
kamchiu <liujinchaona@gmail.com>
2025-09-25 17:03:56 +0800

    change directory

--- setup-ex-bot.sh
+++ setup-ex-bot.sh
@@ -27,8 +27,8 @@ echo "正在复制公钥..."
 ssh-copy-id -i ~/.ssh/id_ed25519.pub $USERNAME@$SERVER_IP
 
 echo "正在复制私钥..."
-scp ~/.ssh/id_ed25519.pub $USERNAME@$SERVER_IP:/home/$USERNAME/.ssh/
-scp ~/.ssh/id_ed25519 $USERNAME@$SERVER_IP:/home/$USERNAME/.ssh/
+scp ~/.ssh/id_ed25519.pub $USERNAME@$SERVER_IP:~/.ssh/
+scp ~/.ssh/id_ed25519 $USERNAME@$SERVER_IP:~/.ssh/
 
 # 2. 连接服务器并执行安装命令
 echo "步骤2: 连接服务器并执行安装..."
